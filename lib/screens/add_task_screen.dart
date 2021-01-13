71b418a4ad6743f079875dcd2830aea43e4c154b
Aashu_Jha <aashish2000kumar@gmail.com>
2021-01-13 19:12:13 +0530

    fix: fixed data null after keyboard enter in add Task button in add_task_screen.dart

--- lib/screens/add_task_screen.dart
+++ lib/screens/add_task_screen.dart
@@ -2,65 +2,76 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_flutter/models/task_data.dart';
 
-class AddTaskScreen extends StatelessWidget {
+class AddTaskScreen extends StatefulWidget {
+  @override
+  _AddTaskScreenState createState() => _AddTaskScreenState();
+}
+
+class _AddTaskScreenState extends State<AddTaskScreen> {
+  TextEditingController testTest = TextEditingController();
+
+  @override
+  void dispose() {
+    testTest.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    String newTaskTitle;
 
     return Container(
       color: Color(0xff757575),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(20.0),
-            topLeft: Radius.circular(20.0),),
-        ),
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).unfocus(),
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 40.0),
-          child: Column(
-            children: [
-              Text('Add Task',
-                style: TextStyle(
-                  color: Colors.lightBlueAccent,
-                  fontSize: 30.0,
-                  fontWeight: FontWeight.w700,
-                ),
-              ),
-              TextField(
-                style: TextStyle(
-                  fontSize: 20.0
-                ),
-                textAlign: TextAlign.center,
-                autofocus: true,
-                onChanged: (newValue){
-                  newTaskTitle = newValue;
-                },
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  FlatButton(
-                    onPressed: () {
-                      Provider.of<TaskData>(context, listen: false).addTask(newTaskTitle);
-                      Navigator.pop(context);
-                    },
-                    child: Text('Add',
-                      style: TextStyle(
-                        fontSize: 15.0,
-                        color: Colors.white,
-                      ),
-                    ),
-                    color: Colors.lightBlue,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20.0),
+              topLeft: Radius.circular(20.0),),
+          ),
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 40.0),
+            child: Column(
+              children: [
+                Text('Add Task',
+                  style: TextStyle(
+                    color: Colors.lightBlueAccent,
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.w700,
                   ),
-                ],
-              ),
-            ],
+                ),
+                TextField(
+                  controller: testTest,
+                  style: TextStyle(
+                    fontSize: 20.0
+                  ),
+                  textAlign: TextAlign.center,
+                  autofocus: true,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    FlatButton(
+                      onPressed: () {
+                        Provider.of<TaskData>(context, listen: false).addTask(testTest.text);
+                        Navigator.pop(context);
+                      },
+                      child: Text('Add',
+                        style: TextStyle(
+                          fontSize: 15.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      color: Colors.lightBlue,
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
     );
   }
-
 }
--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:todo_flutter/models/task_data.dart';
 import '../widgets/tasks_list.dart';
